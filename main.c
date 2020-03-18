39fb3bac2265ca9af3d52b870c716560330d6f0f
Caitlin Millar <c3351208@uon.edu.au>
2020-03-18 17:29:16 +1100

    First attempt

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int x0 = 4; // Initial value
+    int xn; // Next value
+    int xnm1 = x0; // Previous value
+    int n = 0; // Counter
+
+    printf("%d %d\n", n, x0);
+
+    while(n < 10) {
+
+        if(xnm1 > 20) {
+        xn = xnm1/4;
+        }
+        else {
+        xn = 2 * xnm1 + 2;
+        }
+
+    xnm1 = xn;
+    n++;
+    printf("%d %d\n", n, xn);
+
+
+    }
+    return 0;
+}
